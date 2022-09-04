7aec9f0aad436099fa833c4f38fc39de2e664b8d
shintaro <kawaishinta@gmail.com>
2022-09-04 21:48:12 +0900

    add thanks page

--- js/script.js
+++ js/script.js
@@ -150,3 +150,19 @@ $(function () {
     return false;
   });
 });
+
+/* ページ間スムーススクロール 
+========================== */
+$(function () {
+  let url = jQuery(location).attr('href');
+  if (url.indexOf("?id=") == -1) {
+    // スムーズスクロール以外の処理（必要なら）
+  } else {
+    // スムーズスクロールの処理
+    var url_sp = url.split("?id=");
+    var hash = '#' + url_sp[url_sp.length - 1];
+    var tgt = $(hash);
+    var pos = tgt.offset().top - corr_num;
+    $("html, body").animate({ scrollTop: pos }, 400, "swing");
+  }
+});
